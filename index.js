4f579ed24a9551d8ac8b46bc13a4a9c4a187d894
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-11-10 08:20:18 -0500

    basic html files to route urls to

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import http from "http";
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  console.log(req.method);
-
-  res.writeHead(200, { "content-type": "]text/html" });
-  res.end("<h1>Hello World!</h1>");
+  if (req.url === "/") {
+  } else if (req.url === "about") {
+  } else if (req.url === "contact-me") {
+  } else {
+  }
 });
 
-ServiceWorkerRegistration.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
