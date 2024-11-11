00ae4599730a59a0059fdc61eb753c77cfd9d454
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-11-11 07:13:23 -0500

    removed comments;

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-// import {} from "dotenv/config";
-// import fs from "fs/promises";
-// import http from "http";
-// import url from "url";
-// import path from "path";
-// import express from "express";
-
-// const __filename = url.fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
@@ -65,34 +56,3 @@ app.get("*", (req, res) =>
 app.listen(PORT, () => {
   console.log(`Express app running on port ${PORT}`);
 });
-
-// const server = http.createServer(async (req, res) => {
-//   try {
-//     if (req.method === "GET") {
-//       let filePath;
-//       if (req.url === "/") {
-//         filePath = path.join(__dirname, "public", "index.html");
-//       } else if (req.url === "/about") {
-//         filePath = path.join(__dirname, "public", "about.html");
-//       } else if (req.url === "/contact-me") {
-//         filePath = path.join(__dirname, "public", "contact-me.html");
-//       } else {
-//         filePath = path.join(__dirname, "public", "404.html");
-//       }
-
-//       const data = await fs.readFile(filePath);
-//       res.setHeader("Content-Type", "text/html");
-//       res.write(data);
-//       res.end();
-//     } else {
-//       throw new Error("Method not allowed!");
-//     }
-//   } catch (error) {
-//     res.writeHead(500, { "content-type": "text/plain" });
-//     res.end("Server error");
-//   }
-// });
-
-// server.listen(process.env.PORT, () => {
-//   console.log(`Server running on port ${process.env.PORT}`);
-// });
